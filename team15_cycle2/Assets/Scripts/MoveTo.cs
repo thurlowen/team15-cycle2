1e4e528f9a1ebabaa318904c4091b1e92a0dabe3
thurlowen <76582299+thurlowen@users.noreply.github.com>
2023-04-14 22:06:19 +1000

    QOL Changes
    Fixed Script locations, the enemies movement is 0 until phase 2

---
+++ team15_cycle2/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public GameObject AttackLocation;
+    NavMeshAgent agent;
+    private int TimeState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        TimeState = 0;
+        agent.speed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        agent.destination = AttackLocation.transform.position;
+
+        if(TimeState == 1)
+        {
+            agent.speed = 3.5f;
+        }
+
+        if(Input.GetKeyDown("o"))
+        {
+            TimeState--;
+        }
+        if(Input.GetKeyDown("p"))
+        {
+            TimeState++;
+        }
+    }
+}
