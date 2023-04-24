9ad83c860038e154ef42f59df15857ceb9caebcf
Max Padovan <122199194+max-padovan@users.noreply.github.com>
2023-04-24 16:40:54 +1000

    more hotbar work

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Game Reference List
 
 First Person Controller - https://assetstore.unity.com/packages/3d/characters/modular-first-person-controller-189884
+Hotbar Scroller - https://forum.unity.com/threads/how-do-you-get-mouse-scroll-input.825672/
+
--- team15_cycle2/Assets/Scripts/Hotbar.cs
+++ team15_cycle2/Assets/Scripts/Hotbar.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hotbar : MonoBehaviour
 {
@@ -9,9 +10,24 @@ public class Hotbar : MonoBehaviour
     private int maxSlots = 3;
     private int minSlots = 0;
 
+    private GameObject Slot0;
+    private GameObject Slot1;
+    private GameObject Slot2;
+    private GameObject Slot3;
+
     void Start()
     {
-        
+        Slot0 = GameObject.Find("Slot0");
+        Image slot0Image = Slot0.GetComponent<Image>();
+
+        Slot1 = GameObject.Find("Slot1");
+        Image slot1Image = Slot1.GetComponent<Image>();
+
+        Slot2 = GameObject.Find("Slot2");
+        Image slot2Image = Slot2.GetComponent<Image>();
+
+        Slot3 = GameObject.Find("Slot3");
+        Image slot3Image = Slot3.GetComponent<Image>();
     }
 
     void Update()
@@ -34,5 +50,8 @@ public class Hotbar : MonoBehaviour
                 currentSlot = minSlots;
             }
         }
+
+
+        
     }
 }
