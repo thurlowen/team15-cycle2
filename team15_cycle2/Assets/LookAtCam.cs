b5420a7520bf535a655ede72ead205e36a041238
thurlowen <76582299+thurlowen@users.noreply.github.com>
2023-04-25 07:37:48 +1000

    Enemy Health
    Added an enemy health function "EnHealth" which when changed directly updates the health bar on the enemy

--- team15_cycle2/Assets/LookAtCam.cs
+++ team15_cycle2/Assets/LookAtCam.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LookAtCam : MonoBehaviour
 {
     //public Camera cameraToLookAt;
+    
+    private float EnHealth;
+    private float MaxHealth;
+    private Scrollbar slider;
+    
+    void Start()
+    {
+        slider = GetComponentInChildren<Scrollbar>();
+        MaxHealth = 100f;
+        EnHealth = 75f;
+    }
 
     void Update()
     {
+        slider.size = EnHealth/MaxHealth;
         Vector3 v = Camera.main.transform.position - transform.position;
         v.x = v.z = 0.0f;
         transform.LookAt(Camera.main.transform.position - v);
         transform.Rotate(0, 180, 0);
+    }
+
+    public void SetHealth()
+    {
+        //slider.size EnHealth / MaxHeal
     }
 }
