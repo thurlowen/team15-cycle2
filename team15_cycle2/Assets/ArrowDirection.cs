2e5a1cacf89bb5f24903b2ac3e6904a1f3e868de
thurlowen <76582299+thurlowen@users.noreply.github.com>
2023-05-22 14:41:56 +1000

    Arrow Start

---
+++ team15_cycle2/Assets/ArrowDirection.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowDirection : MonoBehaviour
+{
+    private GameObject EndTree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndTree = GameObject.FindGameObjectWithTag("EndTree");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(90,0,0);
+        transform.LookAt(EndTree.transform);
+    }
+}
