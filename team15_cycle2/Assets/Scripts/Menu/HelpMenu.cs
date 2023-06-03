2cfce0019a2aad62b7ebf2ebf347afb9b34fb040
Max Padovan <122199194+max-padovan@users.noreply.github.com>
2023-06-03 13:43:09 +1000

    Help Menu

--- team15_cycle2/Assets/Scripts/Menu/HelpMenu.cs
+++ team15_cycle2/Assets/Scripts/Menu/HelpMenu.cs
@@ -5,7 +5,16 @@ using UnityEngine.UI;
 
 public class HelpMenu : MonoBehaviour
 {
-    private Canvas HM;
+    //private Canvas HM;
+
+    public GameObject TabButton1;
+    public GameObject TabButton2;
+    public GameObject TabButton3;
+
+    public GameObject TabContents1;
+    public GameObject TabContents2;
+    public GameObject TabContents3;
+
     void Start()
     {
         //HM = gameObject.GetComponent<Canvas>();
@@ -13,9 +22,40 @@ public class HelpMenu : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void HideAllTabs()
+    {
+        TabContents1.SetActive(false);
+        TabContents2.SetActive(false);
+        TabContents3.SetActive(false);
+
+        TabButton1.GetComponent<Button>().image.color = new Color32(0, 0, 0, 240);
+        TabButton2.GetComponent<Button>().image.color = new Color32(0, 0, 0, 240);
+        TabButton3.GetComponent<Button>().image.color = new Color32(0, 0, 0, 240);
+    }
+
+    public void ShowTab1()
+    {
+        HideAllTabs();
+        TabContents1.SetActive(true);
+        TabButton1.GetComponent<Button>().image.color = new Color32(0, 0, 0, 250);
+    }
+
+    public void ShowTab2()
+    {
+        HideAllTabs();
+        TabContents2.SetActive(true);
+        TabButton2.GetComponent<Button>().image.color = new Color32(0, 0, 0, 250);
+    }
+
+    public void ShowTab3()
+    {
+        HideAllTabs();
+        TabContents3.SetActive(true);
+        TabButton3.GetComponent<Button>().image.color = new Color32(0, 0, 0, 250);
     }
 }
